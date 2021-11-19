6e206008ab2b68280d03d9c32d0318c9f018927a
Johan <johanfer_12@hotmail.com>
2021-11-18 20:11:12 -0500

    Added FB friends backup and Twitter API

--- StillFriends.py
+++ StillFriends.py
@@ -2,6 +2,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from datetime import datetime
+import itertools
+import tweepy
 import time
 import os
 
@@ -9,18 +12,21 @@ import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 #https://m.facebook.com/(your_username))/friends in urls.txt
+#C:\\Users\\Your_User\\AppData\\Local\\Google\\Chrome\\User Data in urls.txt
 
 urls = open("urls.txt").read().splitlines()
-fb = urls[0]
 options = webdriver.ChromeOptions()
-options.add_argument("user-data-dir=C:\\Users\\Johan\\AppData\\Local\\Google\\Chrome\\User Data")
+options.add_argument("user-data-dir=" + urls[1])
 s=Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s, options=options)
-driver.get(fb)
+driver.get(urls[2])
 
+##Getting current date and time
+today = datetime.now()
+    
 ##Scrolling down the page
 
-SCROLL_PAUSE_TIME = 0.5
+SCROLL_PAUSE_TIME = 1
 
 # Get scroll height
 last_height = driver.execute_script("return document.body.scrollHeight")
@@ -38,7 +44,7 @@ while True:
         break
     last_height = new_height
 
-time.sleep(5)
+time.sleep(3)
 
 #Loading friend list
 
@@ -46,13 +52,76 @@ Friends = driver.find_elements(By.XPATH,"//h3[contains(@class,'_52jh _5pxc _8yo0
 Friends2 = driver.find_elements(By.XPATH,"//h1[contains(@class,'_52jh _5pxc _8yo0')]")
 
 count = 0
+fblist = []
 
-for friend in Friends:
-    print(friend.text)
-    count = count + 1
+for friend in itertools.chain(Friends, Friends2):
+    fblist.append(friend.text)
 
-for friend in Friends2:
-    print(friend.text)
-    count = count + 1
+#Create fb.txt file to store previous results
+if os.path.isfile("fb.txt") == False:
 
-print(count)
+    fb = open('fb.txt', 'a')
+    for friend in fblist:
+        
+        fb.write(friend + '\n')
+        count = count + 1
+
+    fb.close()
+    print("FB file created")
+
+#Compare previous results with current results
+
+else:
+
+    fbback = open('fb.txt').read().splitlines()
+    if fbback == fblist:
+        print("No new friends")
+    if fbback != fblist:
+        difference = list(set(fblist) - set(fbback))
+        print("New friends: " + str(len(difference)))
+        print("Lost friends: " + str(len(list(set(fbback) - set(fblist)))))
+        print("New friends names: " + str(difference))
+        print("Lost friends names: " + str(list(set(fbback) - set(fblist))))
+
+        #Save differences to fbdiff.txt with date and time
+
+        fbdiff = open('fbdiff.txt', 'a')
+        fbdiff.write(str(today) + '\n')
+        #if is a lost friend:
+        for friend in list(set(fbback) - set(fblist)):
+            fbdiff.write("Lost friend: " + friend + '\n')
+        #if is a new friend:
+        for friend in difference:
+            fbdiff.write("New friend: " + friend + '\n')
+        fbdiff.close()
+
+        #update current results to fb.txt and delete previous results:
+
+        fb = open('fb.txt', 'w')
+        for friend in fblist:
+            fb.write(friend + '\n')
+        fb.close()
+
+print(str(len(fblist)) + " friends found!")
+
+####TWITTER FOLLOWERS CHECK####
+
+auth = tweepy.OAuthHandler(urls[5], urls[7])
+auth.set_access_token(urls[11], urls[13])
+api = tweepy.API(auth)
+
+# fetching the user
+user = api.get_user(id=urls[14])
+
+# fetching number of followers
+num_followers = user.followers_count
+
+# fetching all the followers with cursor
+followers = tweepy.Cursor(api.get_followers, id=urls[14]).items()
+
+#print all followers
+count = 0
+for follower in followers:
+    print(follower.screen_name)
+    count += 1
+    print(count)
